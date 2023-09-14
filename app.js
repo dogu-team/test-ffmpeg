60827e8f4a2593c6b3167928047087e53da23736
oneofthezombies <hunhoekim@gmail.com>
2023-09-14 18:40:58 +0900

    chore: change server

--- app.js
+++ app.js
@@ -1,50 +1,55 @@
 const http = require("http");
-const WebSocket = require("ws");
 const ffmpeg = require("ffmpeg-bin-static");
 console.log(ffmpeg.path);
 
 const { spawn } = require("child_process");
 const express = require("express");
-const app = express();
 
-app.get("/download", (req, res) => {
-  res.setHeader("Content-disposition", "attachment; filename=output.mp4");
-  res.setHeader("Content-type", "video/mp4");
-  res.download("./output.mp4");
+const app = express();
+app.use(express.text());
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/record", (req, res) => {
+  const data = req.body.toString();
+  const ffmpegProcess = spawn(`${ffmpeg.path} ${data}`, {
+    shell: true,
+  });
+
+  ffmpegProcess.on("spawn", () => {
+    console.log("spawned");
+  });
+
+  ffmpegProcess.on("error", (err) => {
+    console.log(err);
+  });
+
+  ffmpegProcess.stdout.setEncoding("utf8");
+  ffmpegProcess.stdout.on("data", (data) => {
+    console.log(data.toString());
+  });
+
+  ffmpegProcess.stderr.setEncoding("utf8");
+  ffmpegProcess.stderr.on("data", (data) => {
+    console.log(data.toString());
+  });
+
+  ffmpegProcess.on("close", (code, signal) => {
+    const message = `FFmpeg process closed with code ${code} and signal ${signal}`;
+    console.log(message);
+
+    res.setHeader("Content-disposition", "attachment; filename=output.mp4");
+    res.setHeader("Content-type", "video/mp4");
+    res.download("./output.mp4");
+  });
+
+  setTimeout(() => {
+    ffmpegProcess.kill("SIGINT");
+  }, 5_000);
 });
 
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
-
-wss.on("connection", (ws) => {
-  console.log("WebSocket connected");
-
-  ws.on("message", (message) => {
-    console.log(`Received: ${message}`);
-    ws.send(`You sent: ${message}`);
-
-    const data = message.toString();
-    console.log("Starting ffmpeg");
-    const ffmpegProcess = spawn(`${ffmpeg.path} ${data}`, {
-      stdio: "inherit",
-      shell: true,
-    });
-
-    ffmpegProcess.on("close", (code, signal) => {
-      const message = `FFmpeg process closed with code ${code} and signal ${signal}`;
-      console.log(message);
-      ws.send(message);
-    });
-
-    setTimeout(() => {
-      ffmpegProcess.kill("SIGINT");
-    }, 10000);
-  });
-
-  ws.on("close", () => {
-    console.log("WebSocket disconnected");
-  });
-});
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
